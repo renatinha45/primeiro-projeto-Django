a087ab7b3239c3fe7934a91bb5a68f21acb48157
renata <renata@gmail.com>
2024-08-02 21:14:20 -0300

    config vercel views enquetes

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from. models import Pergunta, Escolha
 from django.urls import reverse
+from django.http import HttpResponse, Http404
 
 
 def index(request):
@@ -26,28 +27,20 @@ def resultados(request, pergunta_id):
     
     
 def votos(request, pergunta_id):
-    Pergunta = get_object_or_404(Pergunta, pk=request.POST["escolha"])
+    Perguntas = get_object_or_404 (Perguntas, pk = pergunta_id)
     try:
-        escolha_selecionada = "pergunta.escolha_set.get(pk=request.POST"["escolha"]
+        escolha_selecionada = pergunta.escolha_set.get (pk = request.POST ["escolha"])
     except (KeyError, Escolha.DoesNotExist):
-
         return render(
             request,
             "enquetes/detalhes.html",
             {
                 "question": Pergunta,
-                "error_message": "Voçe nao selecionou uma escolha",
-           
-            },
-        
+                "error_message" : "você não seçecionou uma escolha"
 
-    )    
+            }
+        )
     else:
-
-        "escolha_selecionada.votos = "F'("votos") + 1
-        "escolha_selecionada".save()
-
-
-
-    "return" HttpResponseRedirect (reverse("enquetes:resultados", args=(Pergunta.id,)))
-
+        escolha_selecionada.votos = F("votos") + 1
+        escolha_selecionada.save()
+    return HttpResponse(f'Você esetá votando na questão {pergunta_id}')
